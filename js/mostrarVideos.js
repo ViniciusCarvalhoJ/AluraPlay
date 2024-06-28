8d7bf13ded918c6ea5e75c44ed41abe4a9cde2a3
Vinicius Carvalho <viniciuscarvalho.dev12@gmail.com>
2024-06-28 15:54:51 -0300

    Modulos 1 e 2 do Curso

---
+++ js/mostrarVideos.js
@@ -0,0 +1,40 @@
+// Importa a função 'conectaApi' do módulo 'conectaApi.js'
+import { conectaApi } from "./conectaApi.js";
+
+// Seleciona o elemento HTML que tem o atributo 'data-lista' e o armazena na constante 'lista'
+const lista = document.querySelector("[data-lista]");
+
+// Função que constrói e retorna um card de vídeo
+function constroiCard(titulo, descricao, url, imagem) {
+    // Cria um elemento <li> para representar um item de lista
+    const video = document.createElement("li");
+    // Adiciona a classe 'videos__item' ao elemento <li>
+    video.classList = 'videos__item';
+    
+    // Define o conteúdo HTML do elemento <li>, incluindo um iframe do YouTube e a descrição do vídeo
+    video.innerHTML = `
+        <iframe width="100%" height="72%" src="${url}"
+                title="${titulo}" frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen></iframe>
+        <div class="descricao-video">
+            <img src="${imagem}" alt="logo canal alura">
+            <h3>${titulo}</h3>
+            <p>${descricao}</p>
+        </div>`;
+    
+    // Retorna o elemento <li> completo, que representa o card do vídeo
+    return video;
+}
+
+// Função assíncrona que lista os vídeos
+async function listaVideos() {
+    // Chama a função 'listaVideos' do módulo 'conectaApi' e espera a resposta
+    const listaApi = await conectaApi.listaVideos();
+    lista.innerHTML = ''; // Limpa a lista existente
+    listaApi.forEach(elemento => lista.appendChild(constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)))
+        
+}
+
+// Chamando a função listaVideos para carregar os vídeos
+listaVideos();
